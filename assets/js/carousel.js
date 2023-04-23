a10579cf64f4294086d0f7255c5b01cdb8164cf2
Sebastián Caubet <rscaubet@gmail.com>
2023-04-23 15:58:20 -0300

    Se agregó newcomers, con el nuevo carousel4 en js

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -22,12 +22,18 @@ class Carousel {
 // Crea instancias de carruseles
 //Carrousel de los Bullys
 const carousel1 = new Carousel('carousel1', 'image-bully', 'prev-btn-bully', 'next-btn-bully');
-carousel1.images = ['images/bully/image_01.JPG', 'images/bully/image_02.JPG', 'images/bully/image_03.JPG' ]
+carousel1.images = ['images/bully/image_01.JPG', 'images/bully/image_02.JPG', 'images/bully/image_03.JPG' ];
 
 // Carrousel de los Ingleses
 const carousel2 = new Carousel('carousel2', 'image-english', 'prev-btn-english', 'next-btn-english');
-carousel2.images = ['images/english/image_01.JPG', 'images/english/image_02.JPG', 'images/english/image_03.JPG', 'images/english/image_04.JPG' ]
+carousel2.images = ['images/english/image_01.JPG', 'images/english/image_02.JPG', 'images/english/image_03.JPG', 'images/english/image_04.JPG' ];
 
 //Carrousel de Franceses
 const carousel3 = new Carousel('carousel3', 'image-french', 'prev-btn-french', 'next-btn-french');
-carousel3.images = ['images/french/image_01.JPG','images/french/image_02.JPG','images/french/image_03.JPG']
+carousel3.images = ['images/french/image_01.JPG','images/french/image_02.JPG','images/french/image_03.JPG'];
+
+//Carrousel de Newcomers
+const carousel4 = new Carousel('carousel4', 'image-newcomers', 'prev-btn-newcomers', 'next-btn-newcomers');
+carousel4.images = ['images/newcomers/image_01.jpeg', 'images/newcomers/image_02.jpeg', 'images/newcomers/image_03.jpeg', 
+'images/newcomers/image_04.jpeg', 'images/newcomers/image_05.jpeg', 'images/newcomers/image_06.jpeg', 'images/newcomers/image_07.jpeg', 
+'images/newcomers/image_08.jpeg', 'images/newcomers/image_09.jpeg'];
